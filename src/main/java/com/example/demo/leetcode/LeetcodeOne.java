23307f97b8123f5725cfdd80bbce319f15e68504
lyz4455 <lyz4455@gmail.com>
2021-07-14 19:11:08 +0800

    tree

--- src/main/java/com/example/demo/leetcode/LeetcodeOne.java
+++ src/main/java/com/example/demo/leetcode/LeetcodeOne.java
@@ -687,30 +687,6 @@ public class LeetcodeOne {
      * <p>
      * 返回滑动窗口中的最大值。
      */
-//    static class Solution239 {
-//        public static int[] maxSlidingWindow(int[] nums, int k) {
-//            int length = nums.length;
-//            if (length <= k) {
-//                return new int[]{max(nums)};
-//            }
-//            int n = length + 1 - k;
-//            int[] res = new int[n];
-//            for (int i = 0; i < n; i++) {
-//                res[i] = max(Arrays.copyOfRange(nums, i, i + k));
-//            }
-//            return res;
-//        }
-//
-//        public static int max(int[] nums) {
-//            int temp = Integer.MIN_VALUE;
-//            for (int item : nums) {
-//                if (item > temp) {
-//                    temp = item;
-//                }
-//            }
-//            return temp;
-//        }
-//    }
     static class Solution239 {
         public static int[] maxSlidingWindow(int[] nums, int k) {
             int n = nums.length;
@@ -740,10 +716,80 @@ public class LeetcodeOne {
         }
     }
 
+    /**
+     * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+     * 回溯算法 控制 条件的开始 结束 以及递归部分
+     */
+    static class Solution141 {
+        public static List<List<Integer>> permute(int[] nums) {
+            int length = nums.length;
+            if (length < 1) {
+                return new ArrayList<>();
+            }
+            int depth = 0;
+            Deque<Integer> path = new LinkedList<>();
+            List<List<Integer>> res = new ArrayList<>();
+            boolean[] used = new boolean[length];
+            recursionSection(nums, depth, length, path, res, used);
+            return res;
+        }
+
+        public static void recursionSection(int[] nums, int depth, int length, Deque<Integer> path, List<List<Integer>> res, boolean[] used) {
+            if (depth == length) {
+                res.add(new ArrayList<>(path));
+                return;
+            }
+            for (int i = 0; i < length; i++) {
+                if (!used[i]) {
+                    path.push(nums[i]);
+                    used[i] = true;
+                    recursionSection(nums, depth + 1, length, path, res, used);
+                    path.pop();
+                    used[i] = false;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(Solution239.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+//        System.out.println(Arrays.toString(Solution239.maxSlidingWindow(new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3)));
+        List<List<Integer>> res = Solution141.permute(new int[]{1, 2, 3});
+        System.out.println(res);
     }
+
+    /**
+     * 运用你所掌握的数据结构，设计和实现一个  LRU (最近最少使用) 缓存机制 。
+     * 实现 LRUCache 类：
+     * <p>
+     * LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+     * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+     * void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+     */
+    class LRUCache extends LinkedHashMap {
+
+        private int capacity;
+
+        public LRUCache(int capacity) {
+            super(capacity, 0.75f, true);
+            this.capacity = capacity;
+        }
+
+        public int get(int key) {
+            return (int) super.getOrDefault(key, -1);
+
+        }
+
+        public void put(int key, int value) {
+            put(key, value);
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry eldest) {
+            return size() > capacity;
+        }
+    }
+
 
 }
 
