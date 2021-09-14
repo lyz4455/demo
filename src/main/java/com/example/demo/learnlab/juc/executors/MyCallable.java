7231a583125a6a0f924f9f20efb7e932bfe509d9
lyz4455 <lyz4455@gmail.com>
2021-09-14 19:12:38 +0800

    callable

--- src/main/java/com/example/demo/learnlab/juc/executors/MyCallable.java
+++ src/main/java/com/example/demo/learnlab/juc/executors/MyCallable.java
@@ -1,6 +1,7 @@
 package com.example.demo.learnlab.juc.executors;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 /**
@@ -25,10 +26,13 @@ public class MyCallable implements Callable {
         return i; //call方法可以有返回值
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         // futureTask是 callable和runnable之间的桥梁
         MyCallable callable = new MyCallable();
         FutureTask futureTask = new FutureTask(callable);
         new Thread(futureTask).start();
+        System.out.println("get阻塞前");
+        System.out.println(futureTask.get());
+        System.out.println("get阻塞后");
     }
 }
